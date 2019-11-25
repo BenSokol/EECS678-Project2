5d76aa02354b4fec13b7c3e9a0b670735053e300
Ben Sokol <ben@bensokol.com>
2019-11-24 18:58:02 -0600

    Update docs

--- docs/libpriqueue_8h.js
+++ docs/libpriqueue_8h.js
@@ -8,6 +8,7 @@ var libpriqueue_8h =
     [ "priqueue_offer", "libpriqueue_8h.html#ae2985443dcb2ebb445cac9db0eb71364", null ],
     [ "priqueue_peek", "libpriqueue_8h.html#a0d74474f0f028ea901df0f911cacc813", null ],
     [ "priqueue_poll", "libpriqueue_8h.html#ac1c41474605f008cd94a1c496f9e935b", null ],
+    [ "priqueue_print", "libpriqueue_8h.html#a7ead71b3b1de3062091564187d225bfb", null ],
     [ "priqueue_remove", "libpriqueue_8h.html#a05cfa1c78e90cf7f85e54f0cef97d359", null ],
     [ "priqueue_remove_at", "libpriqueue_8h.html#a90f1af0ffbf73c6d6d46c36b6d335d13", null ],
     [ "priqueue_size", "libpriqueue_8h.html#aca016038e2d6f5db65673bb1f25bc62f", null ]
